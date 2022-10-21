50a81a727677388519e0746af10ca2caa67a4c2b
米満 悠人 <fko2247015@stu.o-hara.ac.jp>
2022-10-21 13:31:30 +0900

    ショットに敵との当たり判定を追加

--- Object/enemy.cpp
+++ Object/enemy.cpp
@@ -34,3 +34,18 @@ void Enemy::draw()
 {
 	DrawTurnGraphF(m_pos.x, m_pos.y, m_handle, true);
 }
+
+float Enemy::getColWidth()
+{
+	float tempX = 0;
+	float tempY = 0;
+	GetGraphSizeF(m_handle, &tempX, &tempY);
+	return tempX;
+}
+float Enemy::getColHeight()
+{
+	float tempX = 0;
+	float tempY = 0;
+	GetGraphSizeF(m_handle, &tempX, &tempY);
+	return tempY;
+}
